f42e479a0a3bef44019bc87dc746d566a7472019
南风 <571691797@qq.com>
2020-08-13 14:22:16 +0800

    开发分支

---
+++ micro-user/src/main/java/com/souther/cloud/MicroUserApplication.java
@@ -0,0 +1,18 @@
+package com.souther.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: souther
+ * @Date: 2020/8/12 15:09
+ * @Description:
+ */
+@SpringBootApplication
+public class MicroUserApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MicroUserApplication.class, args);
+  }
+
+}
