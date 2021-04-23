2522d0229c38b838f064a663698bc8afdfdc8d4a
souther <newnanfeng@gmail.com>
2021-04-23 18:29:57 +0800

    回家搞

--- common.sh
+++ common.sh
@@ -1,13 +1,13 @@
 
 ## 构建项目
-docker build -t registry.cn-hongkong.aliyuncs.com/souther/micro-service/gateway:latest E:\company\yfu\codespace\micro-service\micro-gateway
-docker build -t registry.cn-hongkong.aliyuncs.com/souther/micro-service/user-service:latest E:\company\yfu\codespace\micro-service\micro-user\user-service
-docker build -t registry.cn-hongkong.aliyuncs.com/souther/micro-service/security:latest E:\company\yfu\codespace\micro-service\micro-security
+docker build -t registry.cn-hongkong.aliyuncs.com/souther/gateway:latest D:\sync\myspace\myproject\micro-service\micro-gateway
+docker build -t registry.cn-hongkong.aliyuncs.com/souther/user_service:latest D:\sync\myspace\myproject\micro-service\micro-user\user-service
+docker build -t registry.cn-hongkong.aliyuncs.com/souther/security:latest D:\sync\myspace\myproject\micro-service\micro-security
+docker build -t registry.cn-hongkong.aliyuncs.com/souther/security:latest D:\sync\myspace\myproject\micro-service\micro-security
 
 ## 上传项目
-docker push registry.cn-hongkong.aliyuncs.com/souther/micro-service/gateway:latest
-docker push registry.cn-hongkong.aliyuncs.com/souther/micro-service/user-service:latest
-docker push registry.cn-hongkong.aliyuncs.com/souther/micro-service/security:latest
+docker push registry.cn-hongkong.aliyuncs.com/souther/gateway:latest
+docker push registry.cn-hongkong.aliyuncs.com/souther/user_service:latest
+docker push registry.cn-hongkong.aliyuncs.com/souther/security:latest
 
 mvn clean package -Dmaven.test.skip=true -pl micro-gateway -am
-###
