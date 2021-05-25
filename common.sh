f253694c49f5f5e59950f314e8fd5cc2849bd47d
souther <newnanfeng@gmail.com>
2021-05-25 18:06:10 +0800

    日志

--- common.sh
+++ common.sh
@@ -1,9 +1,9 @@
 
 ## 构建项目
-docker build -t registry.cn-hongkong.aliyuncs.com/souther/gateway:latest D:\sync\myspace\myproject\micro-service\micro-gateway
-docker build -t registry.cn-hongkong.aliyuncs.com/souther/user_service:latest D:\sync\myspace\myproject\micro-service\micro-user\user-service
-docker build -t registry.cn-hongkong.aliyuncs.com/souther/security:latest D:\sync\myspace\myproject\micro-service\micro-security
-docker build -t registry.cn-hongkong.aliyuncs.com/souther/oms_service:latest D:\sync\myspace\myproject\micro-service\micro-oms\oms-service
+docker build -t registry.cn-hongkong.aliyuncs.com/souther/gateway:latest D:\sync\codespace\my\micro-service\micro-gateway
+docker build -t registry.cn-hongkong.aliyuncs.com/souther/user_service:latest D:\sync\codespace\my\micro-service\micro-user\user-service
+docker build -t registry.cn-hongkong.aliyuncs.com/souther/security:latest D:\sync\codespace\my\micro-service\micro-security
+docker build -t registry.cn-hongkong.aliyuncs.com/souther/oms_service:latest D:\sync\codespace\my\micro-service\micro-oms\oms-service
 
 ## 上传项目
 docker push registry.cn-hongkong.aliyuncs.com/souther/gateway:latest
