1264437c889565b9dad080bcfb174ee0e83a7596
souther <newnanfeng@gmail.com>
2021-06-22 18:18:31 +0800

    update

--- micro-search/search-service/src/main/java/com/souther/cloud/conf/RestClientConfig.java
+++ micro-search/search-service/src/main/java/com/souther/cloud/conf/RestClientConfig.java
@@ -1,8 +1,14 @@
 package com.souther.cloud.conf;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -13,10 +19,32 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RestClientConfig {
 
+    @Value("${elasticsearch.clientIp}")
+    private String clientIp;
+
+    @Value("${elasticsearch.httpPort}")
+    private int httpPort;
+
+    @Value("${elasticsearch.username}")
+    private String username;
+
+    @Value("${elasticsearch.password}")
+    private String password;
+
     public RestHighLevelClient esClient() {
+
+        //认证
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(
-                        new HttpHost("elasticsearch", 9200, "http")));
+                        new HttpHost(clientIp, httpPort, "http"))
+                        .setHttpClientConfigCallback(
+                                (HttpAsyncClientBuilder httpAsyncClientBuilder) ->
+                                        httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
+                        )
+        );
         return client;
     }
 
